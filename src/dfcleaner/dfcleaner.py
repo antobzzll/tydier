686f80159d6ed3b415e3f599a973000acb6ac52c
Antonio Buzzelli <106315448+antobzzll@users.noreply.github.com>
2022-09-15 19:21:38 +0200

    main

--- src/dfcleaner/dfcleaner.py
+++ src/dfcleaner/dfcleaner.py
@@ -1,51 +1,62 @@
 # import numpy as np
 import pandas as pd
 
+
 def _rec_slice_str(str1, each):
-    
+
     length = len(str1)
     positions = length - 1
 
     if each > length:
-        raise ValueError(f"Invalid each number. Max allowed for provided "\
-                        f"string is {length}")
+        raise ValueError(f"Invalid each number. Max allowed for provided "
+                         f"string is {length}")
     if each < 2:
         raise ValueError(f"Invalid each number. Min allowed is 2")
 
-    start = 0; end = each
+    start = 0
+    end = each
     res = list()
     for _ in range(positions):
         chunk = str1[start:end]
         if len(chunk) == each:
             res.append(chunk)
-            start += 1; end += 1
-    
+            start += 1
+            end += 1
+
     return res
 
 
-    
 def str_match_ratio(str1: str, str2: str, method: str,
                     case_sensitive: bool = False):
-    
-    if not case_sensitive: str1 = str1.lower(); str2 = str2.lower()
-        
+
+    if not case_sensitive:
+        str1 = str1.lower()
+        str2 = str2.lower()
+
     if method == 'charbychar':
         len1 = len(str1)
         len2 = len(str2)
         max_len = len1 if len1 > len2 else len2
-        
+
         if len1 < max_len:
             str1 = str1 + "*" * (max_len - len1)
         if len2 < max_len:
             str2 = str2 + "*" * (max_len - len2)
-        
+
         same_chars = 0
         for x, y in zip(str1, str2):
             if x == y:
                 same_chars += 1
-        
+
         return same_chars / max_len
-    
+
+    elif method == 'commonchars':
+        s1 = set(str1)
+        s2 = set(str2)
+        common_chars = s1 & s2
+
+        return len(common_chars)
+
     elif 'sliceeach' in method:
         try:
             each = int(method[-1])
@@ -54,17 +65,16 @@ def str_match_ratio(str1: str, str2: str, method: str,
         grouped1 = _rec_slice_str(str1, each)
         grouped2 = _rec_slice_str(str2, each)
         longest = grouped1 if grouped1 > grouped2 else grouped2
-        
+
         matching_chunks = []
         for chunk in grouped2:
             if chunk in grouped1:
                 matching_chunks.append(chunk)
 
         return len(matching_chunks) / len(longest)
-    
+
     else:
         raise ValueError("Invalid method.")
-
 
 
 def remove_chars(obj_series: pd.Series, chars: list):
@@ -77,12 +87,12 @@ def remove_chars(obj_series: pd.Series, chars: list):
     Returns:
         pd.Series: Cleaned object series
     """
-    for c in chars: obj_series = obj_series.str.replace(c, '')
+    for c in chars:
+        obj_series = obj_series.str.replace(c, '')
     return obj_series
 
 
-    
-def find_inconsistent_categories(series: pd.Series, 
+def find_inconsistent_categories(series: pd.Series,
                                  categories: pd.Series,
                                  mapping_dict: bool = False):
     """Spots incorrect categories in a categorical variable, by checking it
@@ -99,7 +109,7 @@ def find_inconsistent_categories(series: pd.Series,
         dict: Set of non-permitted categories
     """
     diff = list(set(series).difference(categories))
-    
+
     if mapping_dict:
 
         mapping = {}
@@ -108,22 +118,34 @@ def find_inconsistent_categories(series: pd.Series,
             tofix_ls = []
             cat_ls = []
             match_ratio_ls = []
-            
+
             for cat in categories:
                 tofix_ls.append(tofix)
                 cat_ls.append(cat)
-                match_ratio_ls.append(str_match_ratio(tofix, cat, 'sliceeach2'))
-            
-            res = pd.DataFrame({'tofix': tofix_ls, 'cat': cat_ls, 
+                match_ratio_ls.append(str_match_ratio(tofix, cat,
+                                                      case_sensitive=False,
+                                                      method='sliceeach2'))
+
+            res = pd.DataFrame({'tofix': tofix_ls, 'cat': cat_ls,
                                 'match_ratio': match_ratio_ls})
-            
+
             max_ratio = res['match_ratio'].max()
-            # print(res)
-            
-            replacement = res.loc[res['match_ratio'] == max_ratio]['cat'].item()
+            print(max_ratio)
+            print(res)
+
+            try:
+                replacement = res.loc[res['match_ratio']
+                                      == max_ratio]['cat'].item()
+            except ValueError:
+                pot_replacements = res.loc[res['match_ratio']
+                                           == max_ratio]['cat'].to_list()
+                for rep in pot_replacements:
+                    print(tofix, rep, str_match_ratio(
+                        rep, tofix, method='commonchars'))
+            print(replacement)
             mapping[tofix] = replacement
-            
+
         return mapping
-    
+
     else:
         return diff if diff else False
--- tests/demo.py
+++ tests/demo.py
@@ -3,27 +3,30 @@ import dfcleaner
 
 if __name__ == "__main__":
 
-    df = pd.DataFrame({'A': ['Monday', 'Tuesday', 'Wdnesday', 'Thursday', 'Friay', 'Saturday', 'Sunday'], 
+    df = pd.DataFrame({'A': ['Monday', 'Tuesday', 'Wdnesday', 'Thursday', 'Friay', 'Saturday', 'Sunday'],
                        'B': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']})
-    
-    clean_cats = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    
+    dirty_cats = ['Monday', 'Tusday', 'Wednesday',
+                  'Thursdy', 'Friday', 'Saturday', 'Sunday', 'mnday']
+    clean_cats = ['Monday', 'Tuesday', 'Wednesday',
+                  'Thursday', 'Friday', 'Saturday', 'Sunday']
+    print(dfcleaner.str_match_ratio("Monday", "mnday", method='commonchars'))
+
     print(f"\n{'*' * 5} dfcleaner.str_match_ratio: method = 'charbychar'")
     df['MatchRatio_charbychar'] = df.apply(
         lambda row: dfcleaner.str_match_ratio(row['A'], row['B'],
-                                              method = 'charbychar'), axis=1)
+                                              method='charbychar'), axis=1)
     print(df)
-    
+
     print(f"\n{'*' * 5} dfcleaner.str_match_ratio: method = 'sliceeach[n]'")
     df['MatchRatio_sliceeach[n]'] = df.apply(
-        lambda row: dfcleaner.str_match_ratio(row['A'], row['B'], 
-                                              method = 'sliceeache', 
+        lambda row: dfcleaner.str_match_ratio(row['A'], row['B'],
+                                              method='sliceeach2',
                                               case_sensitive=False), axis=1)
     print(df)
-    
+
     print(f"\n{'*' * 5} dfcleaner.remove_chars")
     print(dfcleaner.remove_chars(df['A'], ['c', 'a']))
-    
+
     print(f"\n{'*' * 5} dfcleaner.find_inconsistent_categories")
-    print(dfcleaner.find_inconsistent_categories(df['A'], clean_cats, 
+    print(dfcleaner.find_inconsistent_categories(dirty_cats, clean_cats,
                                                  mapping_dict=True))
